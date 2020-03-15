ad50ad5f2f42f71c004349199e945ae40103d96a
cande1gut <candelario.gtz@gmail.com>
2020-03-15 01:17:25 -0600

    app without sensor readings

--- www/js/app.js
+++ www/js/app.js
@@ -1,48 +1,52 @@
 var $$ = Dom7;
 
+//Sens object info
 var metawear = {
   deviceId: "F0:25:C2:74:09:A1",
   serviceUUID: "326a9000-85cb-9195-d9dd-464cfbbae75a",
   txCharacteristic: "326a9001-85cb-9195-d9dd-464cfbbae75a",
   rxCharacteristic: "326a9006-85cb-9195-d9dd-464cfbbae75a"
 };
+//Sens object info
 
+//Grip object info
+var grip = {
+  deviceId: "",
+  serviceUUID: "",
+  txCharacteristic: "",
+  rxCharacteristic: ""
+};
+//Grip object info
 
+//Plates object info
+var rPlate = {
+  deviceId: "",
+  serviceUUID: "",
+  txCharacteristic: "",
+  rxCharacteristic: ""
+};
 
+var lPlate = {
+  deviceId: "",
+  serviceUUID: "",
+  txCharacteristic: "",
+  rxCharacteristic: ""
+};
+//Plates object info
+
+//F7 initialization
 var app = new Framework7({
   root: '#app', // App root element
   name: 'KRehab', // App name
   theme: 'auto', // Automatic theme detection
-  // App root data
-  data: function () {
-    return {
-      user: {
-        firstName: 'John',
-        lastName: 'Doe',
-      },
-      // Demo products for Catalog section
-      products: [
-        {
-          id: '1',
-          title: 'Apple iPhone 8',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
-        },
-        {
-          id: '2',
-          title: 'Apple iPhone 8 Plus',
-          description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
-        },
-        {
-          id: '3',
-          title: 'Apple iPhone X',
-          description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
-        },
-      ]
-    };
-  },
   routes: routes,
 });
+var mainView = app.views.create('.view-main', {
+  stackPages: true
+});
+//F7 initialization
 
+//Sens object initialization, pairing and reading
 var sens = {
     /*bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
@@ -75,7 +79,7 @@ var sens = {
         sens.connect(device.id);
       }
     },
-    connect: function(id) {
+    connect: function(id){
       sens.deviceId = id;
 
       var onConnect = function() {
@@ -85,32 +89,42 @@ var sens = {
 
       ble.connect(sens.deviceId, onConnect, sens.onError);
     },
-    onData: function(buffer) { // data received from MetaWear
+    onData: function(buffer){ // data received from MetaWear
       var data = new Uint8Array(buffer);
       var message = "";
 
       if (data[0] === 1 && data[1] === 1) { // module = 1, opscode = 1
-          if (data[2] === 1) { // button state
-              message = "Button pressed";
-          } else {
-              message = "Button released";
-          }
+        if (data[2] === 1) { // button state
+            message = "Button pressed";
+        } else {
+            message = "Button released";
+        }
       }
 
       $$("#sens-data").append(message);
       //console.log(message);
 
       //resultDiv.innerHTML = resultDiv.innerHTML + message + "<br/>";
+      $$("#sens-onData").append(
+        "<br>" + message + "<br/>"
+      );
       //resultDiv.scrollTop = resultDiv.scrollHeight;
     },
-    writeData: function(buffer, success, failure) { // to to be sent to MetaWear
+    writeData: function(buffer, success, failure){ // to to be sent to MetaWear
       if (!success)
       {
-        success = function() {
-          console.log("success");
-          //resultDiv.innerHTML = resultDiv.innerHTML + "Sent: " + JSON.stringify(new Uint8Array(buffer)) + "<br/>";
+        success = function(){
+
+          $$("#sens-write").append(
+            "<br>Sent: " + JSON.stringify(new Uint8Array(buffer)) + "<br/>"
+          );
           //resultDiv.scrollTop = resultDiv.scrollHeight;
         };
+      }
+      else{
+        $$("#sens-success").append(
+          JSON.stringify(new Uint8Array(buffer)) + "<br/>"
+        );
       }
 
       if(!failure)
@@ -123,15 +137,15 @@ var sens = {
     subscribeForIncomingData: function(){
       ble.startNotification(sens.deviceId, metawear.serviceUUID, metawear.rxCharacteristic, sens.onData, sens.onError);
     },
-    enableButtonFeedback: function(success, failure) {
-        var data = new Uint8Array(6);
-        data[0] = 0x01; // mechanical switch
-        data[1] = 0x01; // switch state ops code
-        data[2] = 0x01; // enable
+    enableButtonFeedback: function(success, failure){
+      var data = new Uint8Array(6);
+      data[0] = 0x01; // mechanical switch
+      data[1] = 0x01; // switch state ops code
+      data[2] = 0x01; // enable
 
-        sens.writeData(data.buffer, success, failure);
+      sens.writeData(data.buffer, success, failure);
     },
-    /*onMotorButton: function(event) {
+    onMotorButton: function(event){
         var pulseWidth = pulseWidthInput.value;
         var data = new Uint8Array(6);
         data[0] = 0x07; // module
@@ -141,9 +155,9 @@ var sens = {
         data[4] = pulseWidth >> 8; // Pulse Width
         data[5] = 0x00; // Some magic bullshit
 
-        app.writeData(data.buffer);
+        sens.writeData(data.buffer);
     },
-    onBuzzerButton: function(event) {
+    onBuzzerButton: function(event){
         var pulseWidth = pulseWidthInput.value;
         var data = new Uint8Array(6);
         data[0] = 0x07; // module
@@ -153,11 +167,11 @@ var sens = {
         data[4] = pulseWidth >> 8; // Pulse Width
         data[5] = 0x01; // Some magic?
 
-        app.writeData(data.buffer);
+        sens.writeData(data.buffer);
     },
-    disconnect: function(event) {
+    /*disconnect: function(event) {
         ble.disconnect(app.deviceId, app.showMainPage, app.onError);
-        app.deviceId = "";
+        sens.deviceId = "";
     },
     showMainPage: function() {
         mainPage.hidden = false;
@@ -168,17 +182,67 @@ var sens = {
         detailPage.hidden = false;
         resultDiv.innerHTML = "<i>Press the button on the MetaWear</i><br/>";
     },*/
-    onError: function(reason) {
+    onError: function(reason){
       alert("ERROR: " + reason); // real apps should use notification.alert
     }
 };
+//Sens object initialization, pairing and reading
 
-$$(document).on('deviceready', function() {
-  sens.refreshDeviceList();
+//Grip object initialization, pairing and reading
+
+//Grip object initialization, pairing and reading
+
+//Plates object initialization, pairing and reading
+
+//Plates object initialization, pairing and reading
+
+$$(document).on('deviceready', function(){
+  //sens.refreshDeviceList();
+
+  //login screen
+  $$("#login-screen #login-button").on('click', function(){
+    //Login Auth
+    mainView.router.navigate({
+      name: 'calibration',
+    });
+    //Login Auth
+    //console.log(mainView.router.history);
+  });
+  //login screen
+
+  //calibration screen
+  app.on("cardOpen", function(expendableCard){
+    $$("#calibration-fab").hide();
+    if($$("#calibration-page").scrollTop() == 0){
+      app.navbar.toggleLargeTitle("#calibration-nav");
+    }
+    $$("#"+expendableCard.id+"-save").on('click', function(){
+      $$("#"+expendableCard.id+"-icon").remove();
+      $$("#"+expendableCard.id+"-col").append('<i id='+expendableCard.id+'-icon class="calibration-icon icon f7-icons color-green">checkmark_alt_circle_fill</i>');
+      $$("#"+expendableCard.id+"-save").addClass("disabled");
+      $$("#"+expendableCard.id+"-reset").removeClass("disabled");
+    });
+    $$("#"+expendableCard.id+"-reset").on('click', function(){
+      $$("#"+expendableCard.id+"-icon").remove();
+      $$("#"+expendableCard.id+"-col").append('<i id='+expendableCard.id+'-icon class="calibration-icon icon f7-icons color-red">multiply_circle_fill</i>');
+      $$("#"+expendableCard.id+"-reset").addClass("disabled");
+      $$("#"+expendableCard.id+"-save").removeClass("disabled");
+    });
+  });
+
+  app.on("cardClose", function(expendableCard){
+    $$("#calibration-fab").show();
+    if($$("#calibration-page").scrollTop() == 0){
+      app.navbar.toggleLargeTitle("#calibration-nav");
+    }
+  });
+  //calibration screen
+
+
+
   console.log("Device is ready!");
 });
 
-// Login Screen Demo
-$$('#my-login-screen .login-button').on('click', function () {
-
+$$("#motor").on('click', function(){
+  //sens.onMotorButton();
 });
